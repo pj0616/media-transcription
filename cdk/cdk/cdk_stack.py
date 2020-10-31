7602cc942e0a9090c87034e9084a8d2f1db3ad84
parth <prparikh02@gmail.com>
2020-10-30 22:38:06 -0400

    Add SQS queue for receiving S3 event notifications

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     core,
     aws_s3 as s3,
+    aws_s3_notifications as s3n,
+    aws_sqs as sqs,
 )
 
 
@@ -14,8 +16,31 @@ class MediaTranscriptionStack(core.Stack):
         core.Tags.of(self).add('project', 'MediaTranscription')
 
         # Media files bucket
-        s3.Bucket(
+        media_bucket = s3.Bucket(
             self,
             'media-transcription-bucket',
             encryption=s3.BucketEncryption.S3_MANAGED,
+        )
+
+        # SQS queue for media files bucket event notifications
+        media_bucket_event_queue = sqs.Queue(
+            self,
+            'media-transcription-event-notification-queue',
+            queue_name='media-transcription-event-notification-queue',
+            visibility_timeout=core.Duration.seconds(60),
+            dead_letter_queue=sqs.DeadLetterQueue(
+                max_receive_count=3,
+                queue=sqs.Queue(
+                    self,
+                    'media-transcription-event-notifications-dlq',
+                    queue_name='media-transcription-event-notifications-dlq',
+                )
+            ),
+        )
+
+        # S3 object created notifications sent to SQS queue
+        media_bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            s3n.SqsDestination(media_bucket_event_queue),
+            *[s3.NotificationKeyFilter(prefix='media-input/')],
         )
