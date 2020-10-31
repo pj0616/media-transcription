94c486abc8d9bf8addcb50f7985ab542ae3dab4a
parth <prparikh02@gmail.com>
2020-10-30 21:53:57 -0400

    Create S3 Bucket; Include Tag aspect

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -9,4 +9,13 @@ class MediaTranscriptionStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Tag all constructs with the project for easy billing drilldown,
+        # filtering, and organization.
+        core.Tags.of(self).add('project', 'MediaTranscription')
+
+        # Media files bucket
+        s3.Bucket(
+            self,
+            'media-transcription-bucket',
+            encryption=s3.BucketEncryption.S3_MANAGED,
+        )
